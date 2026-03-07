114a59ccde2c7572a92f3388b99efde2df86fe43
faisal-shohag <faisalshohagprog@gmail.com>
2026-03-07 06:56:38 +0600

    fixing cors issue for some users 3

--- src/app.ts
+++ src/app.ts
@@ -12,6 +12,16 @@ app.use(cors({
     credentials: true
 }))
 
+// const corsOptions = {
+//   origin: true,
+//   credentials: true,
+//   methods: ["GET","POST","PUT","DELETE","OPTIONS"],
+//   allowedHeaders: ["Content-Type","Authorization"]
+// }
+
+// app.use(cors(corsOptions))
+// app.options("*", cors(corsOptions))
+
 app.use(cookieParser())
 app.use(bodyParser.json())
 app.use(express.json())
